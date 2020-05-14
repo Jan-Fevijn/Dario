50846ba5a3b6383de62555d84634707a1d752f11
DarioDecupere <dariodecuypere@atheneumjanfevijn.be>
2020-05-14 10:42:37 +0200

    algemene site gip en db opdrachten

--- DB/opdrachten/diepvries.sql
+++ DB/opdrachten/diepvries.sql
@@ -24,16 +24,17 @@ DROP TABLE IF EXISTS `houdbaarheid`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `houdbaarheid` (
   `idhoudbaarheid` int(11) NOT NULL AUTO_INCREMENT,
-  `product` int(11) NOT NULL,
+  `inhoud` int(11) NOT NULL,
   `lade` int(11) NOT NULL,
-  `datumIn` date NOT NULL,
-  `datumUit` date NOT NULL,
+  `invriesdatum` date NOT NULL,
+  `gewicht` varchar(45) NOT NULL,
+  `aantal` varchar(45) NOT NULL,
   PRIMARY KEY (`idhoudbaarheid`),
   UNIQUE KEY `idhoudbaarheid_UNIQUE` (`idhoudbaarheid`),
-  KEY `prod_idx` (`product`),
+  KEY `prod_idx` (`inhoud`),
   KEY `lades_idx` (`lade`),
   CONSTRAINT `lades` FOREIGN KEY (`lade`) REFERENCES `lade` (`idlade`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `prod` FOREIGN KEY (`product`) REFERENCES `producten` (`idproducten`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `prod` FOREIGN KEY (`inhoud`) REFERENCES `inhoud` (`idinhoud`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -43,8 +44,34 @@ CREATE TABLE `houdbaarheid` (
 
 LOCK TABLES `houdbaarheid` WRITE;
 /*!40000 ALTER TABLE `houdbaarheid` DISABLE KEYS */;
-INSERT INTO `houdbaarheid` VALUES (1,2,1,'2020-05-12','2020-05-30'),(2,5,1,'2020-05-12','2020-06-12'),(3,6,2,'2020-05-12','2020-06-21'),(4,1,3,'2020-05-12','2020-07-23'),(5,3,4,'2020-05-12','2020-06-14'),(6,4,5,'2020-05-12','2020-05-29');
+INSERT INTO `houdbaarheid` VALUES (1,2,1,'2020-05-30','250g','2'),(2,5,1,'2020-06-12','1kg','3'),(3,6,2,'2020-06-21','250g','1'),(4,1,3,'2020-07-23','150g','1'),(5,3,4,'2020-06-14','350g','2'),(6,4,5,'2020-05-29','150g','2');
 /*!40000 ALTER TABLE `houdbaarheid` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `inhoud`
+--
+
+DROP TABLE IF EXISTS `inhoud`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `inhoud` (
+  `idinhoud` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(45) NOT NULL,
+  `houdbaarheidsdatum` date NOT NULL,
+  PRIMARY KEY (`idinhoud`),
+  UNIQUE KEY `idproducten_UNIQUE` (`idinhoud`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `inhoud`
+--
+
+LOCK TABLES `inhoud` WRITE;
+/*!40000 ALTER TABLE `inhoud` DISABLE KEYS */;
+INSERT INTO `inhoud` VALUES (1,'broccoli','2020-05-29'),(2,'pizza','2020-06-14'),(3,'ijs','2020-06-29'),(4,'kabbeljauw','2020-05-26'),(5,'ribbetjes','2020-05-19'),(6,'pasta','2020-05-25');
+/*!40000 ALTER TABLE `inhoud` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -56,11 +83,11 @@ DROP TABLE IF EXISTS `lade`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `lade` (
   `idlade` int(11) NOT NULL AUTO_INCREMENT,
-  `ladenummer` int(11) NOT NULL,
+  `lade` int(11) NOT NULL,
   PRIMARY KEY (`idlade`),
   UNIQUE KEY `idlade_UNIQUE` (`idlade`),
-  UNIQUE KEY `ladenummer_UNIQUE` (`ladenummer`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1 MAX_ROWS=6;
+  UNIQUE KEY `ladenummer_UNIQUE` (`lade`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1 MAX_ROWS=6;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -72,31 +99,6 @@ LOCK TABLES `lade` WRITE;
 INSERT INTO `lade` VALUES (1,1),(2,2),(3,3),(4,4),(5,5),(6,6);
 /*!40000 ALTER TABLE `lade` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `producten`
---
-
-DROP TABLE IF EXISTS `producten`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `producten` (
-  `idproducten` int(11) NOT NULL AUTO_INCREMENT,
-  `naam` varchar(45) NOT NULL,
-  PRIMARY KEY (`idproducten`),
-  UNIQUE KEY `idproducten_UNIQUE` (`idproducten`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `producten`
---
-
-LOCK TABLES `producten` WRITE;
-/*!40000 ALTER TABLE `producten` DISABLE KEYS */;
-INSERT INTO `producten` VALUES (1,'broccoli'),(2,'pizza'),(3,'ijs'),(4,'kabbeljauw'),(5,'ribbetjes'),(6,'pasta');
-/*!40000 ALTER TABLE `producten` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -107,4 +109,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-05-12 21:21:04
+-- Dump completed on 2020-05-14 10:40:46
--- DB/opdrachten/spellen.sql
+++ DB/opdrachten/spellen.sql
@@ -16,6 +16,39 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `infospellen`
+--
+
+DROP TABLE IF EXISTS `infospellen`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `infospellen` (
+  `idinfospellen` int(11) NOT NULL AUTO_INCREMENT,
+  `spel` int(11) NOT NULL,
+  `uitgever` int(11) NOT NULL,
+  `type` int(11) NOT NULL,
+  PRIMARY KEY (`idinfospellen`),
+  UNIQUE KEY `idinfospellen_UNIQUE` (`idinfospellen`),
+  KEY `spellen_idx` (`spel`),
+  KEY `types_idx` (`type`),
+  KEY `uitgevers_idx` (`uitgever`),
+  CONSTRAINT `spellen` FOREIGN KEY (`spel`) REFERENCES `spellen` (`idspellen`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `uitgevers` FOREIGN KEY (`uitgever`) REFERENCES `uitgever` (`iduitgever`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `types` FOREIGN KEY (`type`) REFERENCES `type` (`idtype`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `infospellen`
+--
+
+LOCK TABLES `infospellen` WRITE;
+/*!40000 ALTER TABLE `infospellen` DISABLE KEYS */;
+INSERT INTO `infospellen` VALUES (1,3,3,1),(2,1,1,2),(3,2,2,3);
+/*!40000 ALTER TABLE `infospellen` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `persoon`
 --
 
@@ -52,8 +85,6 @@ DROP TABLE IF EXISTS `spellen`;
 CREATE TABLE `spellen` (
   `idspellen` int(11) NOT NULL AUTO_INCREMENT,
   `naam` varchar(45) NOT NULL,
-  `maker` varchar(45) NOT NULL,
-  `type` varchar(45) NOT NULL,
   `korte inhoud` varchar(200) DEFAULT NULL,
   `aankoopdatum` date NOT NULL,
   PRIMARY KEY (`idspellen`),
@@ -67,8 +98,59 @@ CREATE TABLE `spellen` (
 
 LOCK TABLES `spellen` WRITE;
 /*!40000 ALTER TABLE `spellen` DISABLE KEYS */;
-INSERT INTO `spellen` VALUES (1,'call of duty','activision','first person shooter','schieten op andere mensen','2019-07-21'),(2,'fortnite','epic games','battle royale','overleven tot je de laatste bent','2019-03-10'),(3,'Gears of war','	Microsoft Game Studios','third persin shooter','buitenaardse wezens vermoorden','2016-04-17');
+INSERT INTO `spellen` VALUES (1,'call of duty','schieten op andere mensen','2019-07-21'),(2,'fortnite','overleven tot je de laatste bent','2019-03-10'),(3,'Gears of war','buitenaardse wezens vermoorden','2016-04-17');
 /*!40000 ALTER TABLE `spellen` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `type`
+--
+
+DROP TABLE IF EXISTS `type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `type` (
+  `idtype` int(11) NOT NULL AUTO_INCREMENT,
+  `genre` varchar(100) NOT NULL,
+  `leeftijd` int(11) NOT NULL,
+  PRIMARY KEY (`idtype`),
+  UNIQUE KEY `idtype_UNIQUE` (`idtype`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `type`
+--
+
+LOCK TABLES `type` WRITE;
+/*!40000 ALTER TABLE `type` DISABLE KEYS */;
+INSERT INTO `type` VALUES (1,'third person shooter',16),(2,'first person shooter',16),(3,'battle royale',12);
+/*!40000 ALTER TABLE `type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `uitgever`
+--
+
+DROP TABLE IF EXISTS `uitgever`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `uitgever` (
+  `iduitgever` int(11) NOT NULL AUTO_INCREMENT,
+  `naam` varchar(75) NOT NULL,
+  PRIMARY KEY (`iduitgever`),
+  UNIQUE KEY `iduitgever_UNIQUE` (`iduitgever`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `uitgever`
+--
+
+LOCK TABLES `uitgever` WRITE;
+/*!40000 ALTER TABLE `uitgever` DISABLE KEYS */;
+INSERT INTO `uitgever` VALUES (1,'activision'),(2,'epic games'),(3,'microsoft');
+/*!40000 ALTER TABLE `uitgever` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -80,17 +162,16 @@ DROP TABLE IF EXISTS `uitlenen`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `uitlenen` (
   `iduitlenen` int(11) NOT NULL AUTO_INCREMENT,
-  `spel` int(11) NOT NULL,
+  `info` int(11) NOT NULL,
   `persoon` int(11) NOT NULL,
   `datum` date NOT NULL,
   PRIMARY KEY (`iduitlenen`),
   UNIQUE KEY `iduitlenen_UNIQUE` (`iduitlenen`),
   KEY `persoon_idx` (`persoon`),
-  KEY `spel_idx` (`spel`),
-  KEY `persoon_spel` (`spel`,`persoon`),
-  CONSTRAINT `spel` FOREIGN KEY (`spel`) REFERENCES `spellen` (`idspellen`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  KEY `spel_idx` (`info`),
+  CONSTRAINT `info` FOREIGN KEY (`info`) REFERENCES `infospellen` (`idinfospellen`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `persoon` FOREIGN KEY (`persoon`) REFERENCES `persoon` (`idpersoon`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -99,7 +180,7 @@ CREATE TABLE `uitlenen` (
 
 LOCK TABLES `uitlenen` WRITE;
 /*!40000 ALTER TABLE `uitlenen` DISABLE KEYS */;
-INSERT INTO `uitlenen` VALUES (1,3,2,'2020-02-02'),(2,1,3,'2020-04-12'),(3,2,1,'2020-05-11'),(4,3,2,'2020-03-02');
+INSERT INTO `uitlenen` VALUES (5,1,2,'2020-05-14'),(6,2,3,'2020-05-14'),(7,3,1,'2020-05-14');
 /*!40000 ALTER TABLE `uitlenen` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -112,4 +193,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-05-12 21:21:57
+-- Dump completed on 2020-05-14 10:41:17
